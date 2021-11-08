62e18c6db124c432dd9d9f3835040cc395e0d879
jtlee <2287781216@qq.com>
2021-11-08 16:45:34 +0800

    写好Money的CSS和HTML部分并完成模块化

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,12 +22,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
+  @extend %outerShadow;
   display: flex;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   flex-direction: row;
   font-size: 12px;
-
   > .item {
     padding: 2px 0;
     width: 33.33333%;
@@ -35,15 +35,13 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
-
     .icon {
       width: 32px;
       height: 32px;
     }
   }
-
-  > .item.selected {
-    color: red;
+  > .item.selected{
+    color: $color-highlight;
   }
 }
 </style>
