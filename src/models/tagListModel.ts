e5935a70d4934ae6834efc9f3b4f6d60fbe381ee
jtlee <2287781216@qq.com>
2021-11-13 16:51:27 +0800

    将tag的string改为{id,name}

---
+++ src/models/tagListModel.ts
@@ -0,0 +1,31 @@
+const localStorageKeyName = 'recordList';
+type tag = {
+  id:string;
+  name:string;
+}
+
+type TagListModel = {
+  data: tag[]
+  fetch: () => tag[]
+  create: (name: string) => 'success' | 'duplicated' //success表示成功，duplicated表示name重复
+  save: () => void
+}
+const tagListModel: TagListModel = {
+  data: [],
+  fetch(){
+    this.data = JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]') ;
+    return this.data;
+  },
+  create(name:string){
+    const names = this.data.map(item => item.name);
+    if(names.indexOf(name)>=0){return 'duplicated';}
+    this.data.push({id:name ,name:name});
+    this.save;
+    return 'success';
+  },
+  save(){
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
+  }
+};
+
+export default tagListModel;
