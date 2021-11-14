f7023a975da930929c46a4dc1085ee5f476ee950
jtlee <2287781216@qq.com>
2021-11-14 14:05:24 +0800

    最新

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -12,27 +12,22 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component,Prop,Watch} from 'vue-property-decorator';
-
+import {Component, Prop, Watch} from 'vue-property-decorator';
 @Component
-export default class FormItem extends Vue{
-  @Prop({required:true}) value!: string;
-
-  @Prop({required:true}) fieldName!: string;
-  @Prop() placeholder?: string;//?指有可能不存在
-
+export default class FormItem extends Vue {
+  @Prop({default: ''}) readonly value!: string;
+  @Prop({required: true}) fieldName!: string;
+  @Prop() placeholder?: string;
   @Watch('value')
   onValueChanged(value: string) {
     this.$emit('update:value', value);
   }
 }
-
 </script>
 
 <style lang="scss" scoped>
 .formItem {
   font-size: 14px;
-  background: #f5f5f5;
   padding-left: 16px;
   display: flex;
   align-items: center;
