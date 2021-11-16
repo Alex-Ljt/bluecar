7240b20a464c46926339fb3dad62179dd802b41a
jtlee <2287781216@qq.com>
2021-11-16 21:07:16 +0800

    Money.vue使用Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -16,10 +16,16 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-import store from '@/store/index2';
-@Component
+
+@Component({
+  computed:{
+    tagList(){
+      //return tagList = store.fetchTags();
+    }
+  }
+})
 export default class Tags extends Vue {
-  tagList = store.fetchTags();
+
   selectedTags: string[] = [];
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
@@ -35,7 +41,7 @@ export default class Tags extends Vue {
     if (!name) {
       return window.alert('标签名不能为空');
     }
-    store.createTag(name)
+    //store.createTag(name)
     }
 }
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,27 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import clone from '@/lib/clone';
 
 Vue.use(Vuex)//把 store 绑到 Vue.prototype.$store = store
 
 const  store = new Vuex.Store({
-  state: {
-    count:0//data
+  state: {//data
+    recordList:[] as RecordItem[]
   },
   mutations: {//methods
-    increment(state,n: number){
-      state.count += n;
-    }
+    fetchRecords(state) {
+      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[]
+    },
+    createRecord(state,record) {
+      const record2: RecordItem = clone(record);
+      record2.createdAt = new Date();
+      state.recordList.push(record2);
+      store.commit('saveRecords')
+    },
+    saveRecords(state) {
+      window.localStorage.setItem('recordList',
+        JSON.stringify(state.recordList));
+    },
   }
 });
 
